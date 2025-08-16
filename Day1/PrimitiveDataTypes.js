dcb23eabc214eb7231bc4fd5c91fbddb8a315dd4
vikashkumaran <vikashkumaran87@gmail.com>
2025-08-16 10:59:58 +0200

    JS - Data Types Sample

---
+++ Day1/PrimitiveDataTypes.js
@@ -0,0 +1,10 @@
+let firstName = 'vikash'
+let companyName = "edea"
+let mobileNumber = 1234567890
+let isAutomation = true
+let hasPlaywright
+console.log(firstName + " " + typeof (firstName))
+console.log(companyName + " " + typeof (companyName))
+console.log(mobileNumber + " " + typeof (mobileNumber))
+console.log(isAutomation + " " + typeof (isAutomation))
+console.log(hasPlaywright + " " + typeof (hasPlaywright))
